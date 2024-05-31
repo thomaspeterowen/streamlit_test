047d6305261426e59a0b82712fc261217cc4b707
thomaspeterowen <thomaspeterowen@gmail.com>
2024-05-31 23:11:14 +0100

    test

--- quick_test.py
+++ quick_test.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import matplotlib as plt 
-from bokeh.plotting import figure
+#from bokeh.plotting import figure
 
 
 #st.title('Uber pickups in NYC')
@@ -47,14 +47,14 @@ st.pyplot(fig)
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
 
-p = figure(
-    title='simple line example',
-    x_axis_label='x',
-    y_axis_label='y')
+#p = figure(
+#    title='simple line example',
+#    x_axis_label='x',
+#    y_axis_label='y')
 
-p.line(x, y, legend_label='Trend', line_width=2)
+#p.line(x, y, legend_label='Trend', line_width=2)
 
-st.bokeh_chart(p, use_container_width=True)
+#st.bokeh_chart(p, use_container_width=True)
 
 # Create the selectbox 
 option = st.selectbox(
