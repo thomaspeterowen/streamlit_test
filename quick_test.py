552eb7810b704b9bd41daabd35f28e65577fe850
thomaspeterowen <thomaspeterowen@gmail.com>
2024-06-12 13:43:04 +0200

    test

--- quick_test.py
+++ quick_test.py
@@ -39,7 +39,7 @@ st.dataframe(df)  # Same as st.write(df)
 
 
 arr = np.random.normal(1, 1, size=100) 
-fig, ax = plt.pyplot.subplots() 
+fig, ax = plt.subplots() 
 ax.hist(arr, bins=20) 
  
 st.pyplot(fig) 
